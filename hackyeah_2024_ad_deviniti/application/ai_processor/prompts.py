5fdc23242316d3f416902220cdc50b2aec8938f9
Tomasz Kanka <tomasz.kanka@deviniti.com>
2024-09-28 20:52:32 +0200

    prompty

---
+++ hackyeah_2024_ad_deviniti/application/ai_processor/prompts.py
@@ -0,0 +1,17 @@
+class Prompts:
+
+    get_address = """
+Dla podanego adresu: 
+%%ADDRESS
+podaj miasto, kod pocztowy, ulicę numer domu, Wojewodztwo, Powiat, Gminę i kod właściwego urzędu skarbowego, odpwiedz w formacie json"""
+
+#jakby nie dał kodu urzedu, to zapytać poniższym pytaniem, wklejając na końcu dane adresowe
+    get_us_uscode = """
+Podaj mi kod urzędu skarbowego dla poniższego adresu:
+Miasto: %%MIASTO
+Kod pocztowy: %%KOD
+Ulica i numer domu: %%ULICA_NR_DOMU
+Województwo: %%WOJEWODZTWO
+Powiat: %%POWIAT
+Gmina: %%GMINA
+"""
