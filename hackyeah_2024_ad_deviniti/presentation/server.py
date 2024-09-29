78986e1b4ea3b135423ddb1f2a96fac3e6958b8d
Tomasz Kanka <tomasz.kanka@deviniti.com>
2024-09-29 10:13:10 +0200

    simple chat

--- hackyeah_2024_ad_deviniti/presentation/server.py
+++ hackyeah_2024_ad_deviniti/presentation/server.py
@@ -4,6 +4,13 @@ from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, Response
 from loguru import logger
+from hackyeah_2024_ad_deviniti.infrastructure.llm_loaders import (
+    get_azure_gpt_4o,
+    get_azure_gpt_4o_mini,
+)
+from langchain_core.messages import HumanMessage, SystemMessage
+
+
 
 from hackyeah_2024_ad_deviniti.application.conversation_service import (
     ConversationService,
@@ -71,3 +78,14 @@ async def pcc3_xml(data: str, kod_urzedu: str, pesel: str) -> Response:
     renderer = PccRenderer()
     xml = renderer.xml(data, kod_urzedu, pesel)
     return Response(content=xml, media_type="application/xml")
+
+
+
+@app.get("/chat")
+async def chat(s : str) -> Any:
+    gpt = get_azure_gpt_4o()
+    x = gpt.invoke([
+                SystemMessage(content="Jesteś doradcą podatkowym i udzielasz informacji na temat podatków. Nie rozmawaiasz na innne tematy."),
+                HumanMessage(content=s),
+            ])
+    return x
